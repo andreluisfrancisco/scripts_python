24aa30a60f4913ebdabaa876fdb958276bc0bc78
André Luis Francisco <andreluis.francisco@gmail.com>
2024-10-17 21:41:53 -0300

    converte moeda

---
+++ verfica_online/verifica_online.py
@@ -0,0 +1,13 @@
+import requests
+
+def verificar_site(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return f'{url} está online'
+        else:
+            return f'{url} está offline'
+    except requests.ConnectionError:
+        return f'{url} não está acessível'
+
+print(verificar_site('https://www.google.com'))
